6fd8f3ed6d8c0efca93bb37087571f43ed1a86e5
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-11-19 10:36:56 +0700

    a

--- publick script/inst-package2.sh
+++ publick script/inst-package2.sh
@@ -5,9 +5,9 @@
 #tambah akun SSH
 wget -O /usr/local/bin/add-ssh-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/add-ssh-user.sh" && chmod +x /usr/local/bin/add-ssh-user
 #tambah akun Vmess
-wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/add-vmess-user.sh" && chmod +x /usr/local/bin/add-vmess-user
+wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/publick%20script/add-vmess2.sh" && chmod +x /usr/local/bin/add-vmess-user
 #tambah Akun Trojan
-wget -O /usr/local/bin/add-trojan-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/add-trojan-user.sh" && chmod +x /usr/local/bin/add-trojan-user
+wget -O /usr/local/bin/add-trojan-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/publick%20script/add-trojan2.sh" && chmod +x /usr/local/bin/add-trojan-user
 
 #auto deleted akun 
 wget -O /usr/local/bin/del-vmess-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/del-vmess-user.sh" && chmod +x /usr/local/bin/del-vmess-user
