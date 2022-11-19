8066f4de0226a1e81cb96d181f11ef2c93ff73ea
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-11-19 15:37:22 +0700

    q

--- setup.sh
+++ setup.sh
@@ -31,8 +31,8 @@ echo -e "$green      Install SSH / WS               $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-#wget https://raw.githubusercontent.com/hidessh99/projek10/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
-wget https://raw.githubusercontent.com/hidessh99/projek10/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+
+wget https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 sleep 2
 clear
 
