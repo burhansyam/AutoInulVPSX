059aa8fb7ce825ba16f6b8dc3d6833a3d7ab9e08
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-11-22 12:46:47 +0700

    a

---
+++ hide/add-shadowsocks-user.sh
@@ -0,0 +1,281 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+
+clear
+source /var/lib/scrz-prem/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/xray/domain)
+else
+domain=$IP
+fi
+
+tls="$(cat ~/log-install.txt | grep -w "Sodosok WS/GRPC" | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "User: " -e user
+		CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+clear
+			echo "A client with the specified name was already created, please choose another name."
+      exit 1
+		fi
+	done
+
+cipher="aes-128-gcm"
+read -p "Password (UUID) : " uuid
+read -p "Expired (Ex. 1 days): " masaaktif
+exp=`date -d "$masaaktif" +"%Y-%m-%d"`
+sed -i '/#ssws$/a\### '"$user $exp"'\
+},{"password": "'""$uuid""'","method": "'""$cipher""'","email": "'""$user""'"' /etc/xray/config.json
+sed -i '/#ssgrpc$/a\### '"$user $exp"'\
+},{"password": "'""$uuid""'","method": "'""$cipher""'","email": "'""$user""'"' /etc/xray/config.json
+echo $cipher:$uuid > /tmp/log
+shadowsocks_base64=$(cat /tmp/log)
+echo -n "${shadowsocks_base64}" | base64 > /tmp/log1
+shadowsocks_base64e=$(cat /tmp/log1)
+shadowsockslink="ss://${shadowsocks_base64e}@$domain:$tls?plugin=xray-plugin;mux=0;path=/hidessh-ss-ws;host=$domain;tls#${user}"
+shadowsockslink1="ss://${shadowsocks_base64e}@$domain:$tls?plugin=xray-plugin;mux=0;serviceName=hidessh-ss-grpc;host=$domain;tls#${user}"
+systemctl restart xray
+rm -rf /tmp/log
+rm -rf /tmp/log1
+
+ssws=$(cat <<ACCOUNT
+{ 
+ "dns": {
+    "servers": [
+      "8.8.8.8",
+      "8.8.4.4"
+    ]
+  },
+ "inbounds": [
+   {
+      "port": 10808,
+      "protocol": "socks",
+      "settings": {
+        "auth": "noauth",
+        "udp": true,
+        "userLevel": 8
+      },
+      "sniffing": {
+        "destOverride": [
+          "http",
+          "tls"
+        ],
+        "enabled": true
+      },
+      "tag": "socks"
+    },
+    {
+      "port": 10809,
+      "protocol": "http",
+      "settings": {
+        "userLevel": 8
+      },
+      "tag": "http"
+    }
+  ],
+  "log": {
+    "loglevel": "none"
+  },
+  "outbounds": [
+    {
+      "mux": {
+        "enabled": true
+      },
+      "protocol": "shadowsocks",
+      "settings": {
+        "servers": [
+          {
+            "address": "$domain",
+            "level": 8,
+            "method": "$cipher",
+            "password": "$uuid",
+            "port": 443
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "security": "tls",
+        "tlsSettings": {
+          "allowInsecure": true,
+          "serverName": "$domain"
+        },
+        "wsSettings": {
+          "headers": {
+            "Host": "$domain"
+          },
+          "path": "/hidessh-ss-ws"
+        }
+      },
+      "tag": "proxy"
+    },
+    {
+      "protocol": "freedom",
+      "settings": {},
+      "tag": "direct"
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {
+        "response": {
+          "type": "http"
+        }
+      },
+      "tag": "block"
+    }
+  ],
+  "policy": {
+    "levels": {
+      "8": {
+        "connIdle": 300,
+        "downlinkOnly": 1,
+        "handshake": 4,
+        "uplinkOnly": 1
+      }
+    },
+    "system": {
+      "statsOutboundUplink": true,
+      "statsOutboundDownlink": true
+    }
+  },
+  "routing": {
+    "domainStrategy": "Asls",
+"rules": []
+  },
+  "stats": {}
+ }
+ACCOUNT
+)
+
+ssgrpc=$(cat <<ACCOUNT
+{
+    "dns": {
+    "servers": [
+      "8.8.8.8",
+      "8.8.4.4"
+    ]
+  },
+ "inbounds": [
+   {
+      "port": 10808,
+      "protocol": "socks",
+      "settings": {
+        "auth": "noauth",
+        "udp": true,
+        "userLevel": 8
+      },
+      "sniffing": {
+        "destOverride": [
+          "http",
+          "tls"
+        ],
+        "enabled": true
+      },
+      "tag": "socks"
+    },
+    {
+      "port": 10809,
+      "protocol": "http",
+      "settings": {
+        "userLevel": 8
+      },
+      "tag": "http"
+    }
+  ],
+  "log": {
+    "loglevel": "none"
+  },
+  "outbounds": [
+    {
+      "mux": {
+        "enabled": true
+      },
+      "protocol": "shadowsocks",
+      "settings": {
+        "servers": [
+          {
+            "address": "$domain",
+            "level": 8,
+            "method": "$cipher",
+            "password": "$uuid",
+            "port": 443
+          }
+        ]
+      },
+      "streamSettings": {
+        "grpcSettings": {
+          "multiMode": true,
+          "serviceName": "hidessh-ss-grpc"
+        },
+        "network": "grpc",
+        "security": "tls",
+        "tlsSettings": {
+          "allowInsecure": true,
+          "serverName": "$domain"
+        }
+      },
+      "tag": "proxy"
+    },
+    {
+      "protocol": "freedom",
+      "settings": {},
+      "tag": "direct"
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {
+        "response": {
+          "type": "http"
+        }
+      },
+      "tag": "block"
+    }
+  ],
+  "policy": {
+    "levels": {
+      "8": {
+        "connIdle": 300,
+        "downlinkOnly": 1,
+        "handshake": 4,
+        "uplinkOnly": 1
+      }
+    },
+    "system": {
+      "statsOutboundUplink": true,
+      "statsOutboundDownlink": true
+    }
+  },
+  "routing": {
+    "domainStrategy": "Asls",
+"rules": []
+  },
+  "stats": {}
+}
+ACCOUNT
+)
+
+result=$(cat <<RESULT
+{
+  "tls": "$shadowsockslink",
+  "ntls": "$shadowsockslink1",
+  "password": "$uuid",
+  "port": "$tls",
+  "method": "aes-128-gcm",
+  "exp": "$exp",
+  "path": "/hidessh-ss-ws"
+}
+RESULT
+)
+
+resultb64=$(  base64 -w 0 <<< $result)
+
+systemctl restart xray > /dev/null 2>&1
+service cron restart > /dev/null 2>&1
+clear
+echo $resultb64
---
+++ hide/pack-hide.sh
@@ -0,0 +1,11 @@
+wget -O /usr/local/bin/add-shadowsocks-user "https://gist.githubusercontent.com/satriaajiputra/0f5538401c50fe06af89e419627077b1/raw/6302f89464e5cc08bf7fe24df4f72cf8cc599317/add-shadowsocks-user.sh" && chmod +x /usr/local/bin/add-shadowsocks-user
+
+wget -O /usr/local/bin/add-trojango-user "https://gist.githubusercontent.com/satriaajiputra/0f5538401c50fe06af89e419627077b1/raw/6302f89464e5cc08bf7fe24df4f72cf8cc599317/add-trojango-user.sh" && chmod +x /usr/local/bin/add-trojango-user
+
+wget -O /usr/local/bin/add-v2ray-user "https://gist.githubusercontent.com/satriaajiputra/0f5538401c50fe06af89e419627077b1/raw/6302f89464e5cc08bf7fe24df4f72cf8cc599317/add-v2ray-user.sh" && chmod +x /usr/local/bin/add-v2ray-user
+
+wget -O /usr/local/bin/del-trojango-user "https://gist.githubusercontent.com/satriaajiputra/0f5538401c50fe06af89e419627077b1/raw/6302f89464e5cc08bf7fe24df4f72cf8cc599317/del-trojango-user.sh" && chmod +x /usr/local/bin/del-trojango-user
+
+wget -O /usr/local/bin/del-v2ray-user "https://gist.githubusercontent.com/satriaajiputra/0f5538401c50fe06af89e419627077b1/raw/6302f89464e5cc08bf7fe24df4f72cf8cc599317/del-v2ray-user.sh" && chmod +x /usr/local/bin/del-v2ray-user
+
+wget -O /usr/local/bin/trvmss-del-exp "https://gist.githubusercontent.com/satriaajiputra/0f5538401c50fe06af89e419627077b1/raw/6302f89464e5cc08bf7fe24df4f72cf8cc599317/trvmss-del-exp.sh" && chmod +x /usr/local/bin/trvmss-del-exp
