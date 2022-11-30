4205aa39828e4256320a9a34db58f70f4f846b27
Burhan Syam <23581412+burhansyam@users.noreply.github.com>
2022-11-30 09:07:01 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━�
 sleep 2
 clear
 
-wget https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 sleep 2
 clear
 
@@ -43,7 +43,7 @@ echo -e "$green Install Vmess,Trojan,Vless,Shadowsoks  $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/hidessh99/tuunnel-mx/main/xray/hideinst-xrayv5.sh && chmod +x hideinst-xrayv5.sh && ./hideinst-xrayv5.sh
+wget https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/hideinst-xrayv5.sh && chmod +x hideinst-xrayv5.sh && ./hideinst-xrayv5.sh
 sleep 2
 clear
 
@@ -53,39 +53,39 @@ echo -e "$green          Install Websocket              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/hidessh99/tuunnel-mx/main/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
+wget https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 sleep 2
 clear
 
 #package tambahan add aaccount
 
 #Vmess/v2ray
-wget -q -O /usr/bin/add-ws "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/add-vmess-user.sh" && chmod +x /usr/bin/add-ws
-wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/hidessh99/projek10/main/cek-user.sh" && chmod +x /usr/bin/cek-ws
-wget -q -O /usr/bin/del-ws "https://raw.githubusercontent.com/bracoli/v4/main/xray/del-ws.sh" && chmod +x /usr/bin/del-ws
-wget -q -O /usr/bin/renew-ws "https://raw.githubusercontent.com/bracoli/v4/main/xray/renew-ws.sh" && chmod +x /usr/bin/renew-ws
+wget -q -O /usr/bin/add-ws "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/add-vmess-user.sh" && chmod +x /usr/bin/add-ws
+wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/cek-user.sh" && chmod +x /usr/bin/cek-ws
+wget -q -O /usr/bin/del-ws "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/del-ws.sh" && chmod +x /usr/bin/del-ws
+wget -q -O /usr/bin/renew-ws "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/renew-ws.sh" && chmod +x /usr/bin/renew-ws
 
 #Vless
-wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/hidessh99/tuunnel-mx/main/xray/hide-add-vless.sh" && chmod +x /usr/bin/add-vless
-wget -q -O /usr/bin/renew-vless "https://raw.githubusercontent.com/bracoli/v4/main/xray/renew-vless.sh" && chmod +x /usr/bin/renew-vless
-wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/bracoli/v4/main/xray/del-vless.sh" && chmod +x /usr/bin/del-vless
-wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/hidessh99/projek10/main/cek-user.sh" && chmod +x /usr/bin/cek-vless
+wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/hide-add-vless.sh" && chmod +x /usr/bin/add-vless
+wget -q -O /usr/bin/renew-vless "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/renew-vless.sh" && chmod +x /usr/bin/renew-vless
+wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/del-vless.sh" && chmod +x /usr/bin/del-vless
+wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/cek-user.sh" && chmod +x /usr/bin/cek-vless
 
 #trojan
-wget -q -O /usr/bin/add-tr "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/add-trojan-user.sh" && chmod +x /usr/bin/add-tr
-wget -q -O /usr/bin/addtrgo "https://raw.githubusercontent.com/hidessh99/tuunnel-mx/main/xray/hide-add-tr.sh" && chmod +x /usr/bin/addtrgo
+wget -q -O /usr/bin/add-tr "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/add-trojan-user.sh" && chmod +x /usr/bin/add-tr
+wget -q -O /usr/bin/addtrgo "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/hide-add-tr.sh" && chmod +x /usr/bin/addtrgo
 
-wget -q -O /usr/bin/del-tr "https://raw.githubusercontent.com/bracoli/v4/main/xray/del-tr.sh" && chmod +x /usr/bin/del-tr
-wget -q -O /usr/bin/renew-tr "https://raw.githubusercontent.com/bracoli/v4/main/xray/renew-tr.sh" && chmod +x /usr/bin/renew-tr
-wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/hidessh99/projek10/main/cek-user.sh" && chmod +x /usr/bin/cek-tr
+wget -q -O /usr/bin/del-tr "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/del-tr.sh" && chmod +x /usr/bin/del-tr
+wget -q -O /usr/bin/renew-tr "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/renew-tr.sh" && chmod +x /usr/bin/renew-tr
+wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/cek-user.sh" && chmod +x /usr/bin/cek-tr
 
 #shadowsoksks
-wget -q -O /usr/bin/add-ssws "https://raw.githubusercontent.com/hidessh99/tuunnel-mx/main/xray/hide-add-ss.sh" && chmod +x /usr/bin/add-ssws
+wget -q -O /usr/bin/add-ssws "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/hide-add-ss.sh" && chmod +x /usr/bin/add-ssws
 
 
-wget -q -O /usr/bin/del-user "https://raw.githubusercontent.com/hidessh99/tuunnel-mx/main/xray/del-ws.sh" && chmod +x /usr/bin/del-user
-wget -q -O /usr/bin/renew-user "https://raw.githubusercontent.com/hidessh99/tuunnel-mx/main/xray/renew-ws.sh" && chmod +x /usr/bin/renew-user
-wget -q -O /usr/bin/crtv2ray "https://raw.githubusercontent.com/hidessh99/tuunnel-mx/main/xray/crt.sh" && chmod +x /usr/bin/crtv2ray
+wget -q -O /usr/bin/del-user "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/del-ws.sh" && chmod +x /usr/bin/del-user
+wget -q -O /usr/bin/renew-user "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/renew-ws.sh" && chmod +x /usr/bin/renew-user
+wget -q -O /usr/bin/crtv2ray "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/crt.sh" && chmod +x /usr/bin/crtv2ray
 
 sleep 2
 
@@ -162,7 +162,7 @@ rm /root/setup.sh >/dev/null 2>&1
 rm /root/ins-xray.sh >/dev/null 2>&1
 rm /root/insshws.sh >/dev/null 2>&1
 cd
-wget https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/hide/pack-hide.sh;chmod +x pack-hide.sh;./pack-hide.sh
+wget https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/hide/pack-hide.sh;chmod +x pack-hide.sh;./pack-hide.sh
 rm -rf updatek.sh
 rm -rf dns-cf.sh
 rm -rf update.sh
