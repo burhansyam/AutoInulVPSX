8be0319db69cf05e67dbb1aa72b7bd0bcb789116
Burhan Syam <23581412+burhansyam@users.noreply.github.com>
2022-12-09 01:09:41 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,15 +2,15 @@
 
 
 #tambah akun SSH
-wget -O /usr/local/bin/add-ssh-user "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/add-ssh-user.sh" && chmod +x /usr/local/bin/add-ssh-user
+wget -O /usr/local/bin/add-ssh-user "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/add-ssh-user.sh" && chmod +x /usr/local/bin/add-ssh-user
 #tambah akun Vmess
-wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/add-vmess-user.sh" && chmod +x /usr/local/bin/add-vmess-user
+wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/add-vmess-user.sh" && chmod +x /usr/local/bin/add-vmess-user
 #tambah Akun Trojan
-wget -O /usr/local/bin/add-trojan-user "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/add-trojan-user.sh" && chmod +x /usr/local/bin/add-trojan-user
+wget -O /usr/local/bin/add-trojan-user "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/add-trojan-user.sh" && chmod +x /usr/local/bin/add-trojan-user
 
 #auto deleted akun 
-wget -O /usr/local/bin/del-vmess-user "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/del-vmess-user.sh" && chmod +x /usr/local/bin/del-vmess-user
-wget -O /usr/local/bin/del-trojan-user "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/del-trojan-user.sh" && chmod +x /usr/local/bin/del-trojan-user
+wget -O /usr/local/bin/del-vmess-user "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/del-vmess-user.sh" && chmod +x /usr/local/bin/del-vmess-user
+wget -O /usr/local/bin/del-trojan-user "https://raw.githubusercontent.com/burhansyam/AutoInulVPSX/main/xray/del-trojan-user.sh" && chmod +x /usr/local/bin/del-trojan-user
 
 #hapus file
 cd
