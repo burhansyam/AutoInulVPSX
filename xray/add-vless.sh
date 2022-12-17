f51926c72f307de3d9b0235d843b6c18c4c59cfd
Burhan Syam <23581412+burhansyam@users.noreply.github.com>
2022-12-17 13:46:34 +0700

    Update add-vless.sh

--- xray/add-vless.sh
+++ xray/add-vless.sh
@@ -47,8 +47,8 @@ sed -i '/#vlessgrpc$/a\#& '"$user $exp"'\
 },{"id": "'""$user""'","email": "'""$user""'"' /etc/xray/config.json
 vlesslink1="vless://${user}@104.18.2.2:$tls?sni=${domain}&host=${domain}&type=ws&security=tls&path=%2Fworryfree&encryption=none#Pendidikan_${user}"
 vlesslink2="vless://${user}@${domain}:$none?host=tsel.me&path=/worryfree&encryption=none&type=ws#Opok_${user}"
-vlesslink3="vless://${user}@104.18.23.187:$none?sni=${domain}&host=${domain}&type=ws&security=none&path=%2Fworryfree&encryption=none#GameMax${user}"
-vlesslink4="vless://${user}@104.17.2.81:$tls?host=${domain}&path=%2Fworryfree&type=ws&encryption=none&security=tls&sni=${domain}#Akrab_{user}"
+vlesslink3="vless://${user}@104.18.23.187:$none?sni=${domain}&host=${domain}&type=ws&security=none&path=%2Fworryfree&encryption=none#GameMax_${user}"
+vlesslink4="vless://${user}@104.17.2.81:$tls?host=${domain}&path=%2Fworryfree&type=ws&encryption=none&security=tls&sni=${domain}#Akrab_${user}"
 systemctl restart xray
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
