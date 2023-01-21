60b822a3c30643ca2e968fabf93821ff6341bda3
Burhan Syam <23581412+burhansyam@users.noreply.github.com>
2023-01-21 15:52:57 +0700

    Update xrayv5.sh

--- xray/xrayv5.sh
+++ xray/xrayv5.sh
@@ -138,7 +138,7 @@ cat > /etc/xray/config.json << END
        "streamSettings":{
          "network": "ws",
             "wsSettings": {
-                "path": "/vmess"
+                "path": "/http://tsel.me/worryfree"
           }
         }
      },
